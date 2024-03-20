fa4175465052dc22118276b7cf47ca860015373d
Kevin <kevxemail@gmail.com>
2024-03-20 18:34:52 -0400

    Implemented initialization, setupRMQConnection, on_message_callback, startConsuming

--- Tech-Lab-On-Campus/Topic-Exchange/consumer_interface.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consumer_interface.py
@@ -11,36 +11,42 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import pika
+import os
 
 class mqConsumerInterface:
     def __init__(self, exchange_name: str) -> None:
         # Save parameters to class variables
+        self.exchange_name = exchange_name
 
         # Call setupRMQConnection
-
-        pass
+        self.setupRMQConnection()
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
-
+        self.con_params = pika.URLParameters(os.environ["AMQP_URL"])
+        self.connection = pika.BlockingConnection(parameters=self.con_params)
         # Establish Channel
-
+        self.channel = self.connection.channel()
         # Create the exchange if not already present
+        self.exchange = self.channel.exchange_declare(exchange="AK-Exchange")
 
-        pass
 
     def bindQueueToExchange(self, queueName: str, topic: str) -> None:
         # Bind Binding Key to Queue on the exchange
-
+        self.channel.queue_bind(
+            queue= "AK-Queue",
+            routing_key= "Routing Key",
+            exchange="AK-Exchange",
+        )
         pass
 
     def createQueue(self, queueName: str) -> None:
         # Create Queue if not already present
+        self.channel.queue_declare(queue="AK-Queue")
 
         # Set-up Callback function for receiving messages
-
-        pass
+        self.channel.basic_consume("AK-Queue", self.on_message_callback, auto_ack=False)
 
     def on_message_callback(self, channel, method_frame, header_frame, body):
         # De-Serialize JSON message object if Stock Object Sent
